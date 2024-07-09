4755e6f1be50331098f1aa0ca27a79efdf82936a
M-Saim786 <saimrashid3344@gmail.com>
2024-07-10 00:01:40 +0500

    blog apis added

--- Controller/BlogController.js
+++ Controller/BlogController.js
@@ -1,12 +1,72 @@
+const BlogandCommentSchema = require("../Model/BlogandCommentSchema");
+
 exports.createBlogPost = async (req, res) => {
     try {
-        const { title, desc } = req.body;
-        if (!title || !desc) {
+        const { title, description } = req.body;
+        if (!title || !description) {
             return res.status(400).json({
-                message: "Title & desc required"
+                message: "Title & description required"
+            })
+        }
+        req.body.authorId = req.user._id;
+        const blog = await BlogandCommentSchema(req.body).save();
+        return res.status(200).json({
+            message: "Blog added successfully",
+            data: blog
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+exports.getAllBlogs = async (req, res) => {
+    try {
+
+        const blogs = await BlogandCommentSchema.find();
+        return res.status(200).json({
+            data: blogs
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+exports.updateBlog = async (req, res) => {
+    try {
+        const blogId = req.params.id;
+        const blog = await BlogandCommentSchema.findOne({ _id: blogId });
+        if (!blog) {
+            return res.status(404).json({
+                message: "Blog not found"
             })
         }
 
+        const updates = {};
+        for (const field in req.body) {
+            if (req.body[field] !== undefined && req.body[field] !== "") {
+                updates[field] = req.body[field];
+            }
+        }
+
+        if (!Object.keys(updates).length) {
+            return res.status(400).json({ message: "No valid update fields provided" });
+        }
+
+        await BlogandCommentSchema.findOneAndUpdate({ _id: blogId }, req.body);
+        return res.status(200).json({
+            message: "Blog Updated"
+        })
+
+
+        return res.status(200).json({
+            data: blogs
+        })
 
     } catch (err) {
         return res.status(500).json({
--- Router/blogRouter.js
+++ Router/blogRouter.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createBlogPost } = require("../Controller/BlogController");
+const { createBlogPost, getAllBlogs, updateBlog } = require("../Controller/BlogController");
 const { protect } = require("../AuthMiddleware/Protect");
 
 router.post("/create", protect, createBlogPost);
+router.get("/", protect, getAllBlogs);
+router.put("/update/:id", protect, updateBlog);
+// router.get("/delete", protect, getAllBlogs);
 
 module.exports = router;
